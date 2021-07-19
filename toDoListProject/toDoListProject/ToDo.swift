487d9720a389d0f0c62e6eb7198e36791ed5180b
IzzyBizzyCoding <87436915+IzzyBizzyCoding@users.noreply.github.com>
2021-07-19 10:50:12 -0400

    To Do List Upload / Iteration 0

---
+++ toDoListProject/toDoListProject/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  toDoListProject
+//
+//  Created by  Scholar on 7/19/21.
+//
+
+import UIKit
+
+class ToDoClass {
+    
+    var description = ""
+    var important = false
+}
